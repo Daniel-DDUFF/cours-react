17d137853bac9dae173ea3f992100f463f410dd9
Abbes Boulebtina <abbes.boulebtina@gmail.com>
2024-10-06 14:49:46 -0400

    Implement ch02 and ch03

---
+++ src/ch03/Button.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const Button = () => {
+  return (
+    <button>envoyer</button>
+  )
+}
+
+export default Button
---
+++ src/ch03/Form.js
@@ -0,0 +1,14 @@
+import React from "react";
+import Button from "./Button";
+
+const Form = () => {
+  return (
+    <>
+      <Button></Button>
+      <Button></Button>
+      <Button></Button>
+    </>
+  );
+};
+
+export default Form;
