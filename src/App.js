23a7571033b626fe41dd4dd1e56057b09c85dda8
Abbes Boulebtina <abbes.boulebtina@gmail.com>
2024-10-09 20:53:51 -0400

    exemple.

--- src/App.js
+++ src/App.js
@@ -5,6 +5,7 @@ import Form from "./ch03/Form";
 import Button1 from "./ch03/Button1";
 import Book1 from "./ch03/Book1";
 import Title3 from "./ch03/Title3";
+import Profile from "./ch03/Profile";
 
 function App() {
   return (
@@ -15,6 +16,7 @@ function App() {
       <Book1>
         <Title3 title="Javascript" author="Abbes" />
       </Book1>
+      <Profile />
     </>
   );
 }
